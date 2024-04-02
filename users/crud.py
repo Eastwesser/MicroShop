f9ec105fac21314a5b7ec73bd4d918c010323420
Dennis Mattews <alttert@mail.ru>
2024-04-02 13:28:20 +0300

    0.0.5 MicroShop CRUD

--- users/crud.py
+++ users/crud.py
@@ -7,7 +7,7 @@ Delete
 from users.schemas import CreateUser
 
 
-def create_user(user_in: CreateUser) -> dict:
+def create_user(user_in: CreateUser) -> dict:  # раньше был dict(), но он сейчас depricated. Используем model_dump()
     user = user_in.model_dump()
     return {
         "success": True,
